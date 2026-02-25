ded3a4be4ffe57fe65ab9862146f7d91e4e0ee3e
dev_fares <kortbifares1@gmail.com>
2026-02-25 15:01:49 +0100

    [DOC] Ajout des docstrings et mise en conformité main.py et utils.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import utils
+
+def Main():
+    notes=[10,14,8,19,13]
+
+    print("Moyenne:",utils.CalculMoyenne(notes))
+    print("Max:",utils.trouverMax(notes))
+    print("Min:",utils.min_value(notes))
+
+Main()
