857b61357bd5327eed24a71660720e4cb359dc24
benakliines07-source <benakliines07@gmail.com>
2026-02-25 15:26:12 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,42 @@
 # student statistics utilities
 
-def CalculMoyenne(ListeNotes):
-    somme=0
-    for n in ListeNotes:
-        somme=somme+n
-    moyenne=somme/len(ListeNotes)
+def calcul_moyenne(liste_notes):
+    """
+    Calcule et retourne la moyenne d'une liste de notes.
+    Retourne None si la liste est vide.
+    """
+    if not liste_notes:
+        return None
+    
+    somme = 0
+    for note in liste_notes:
+        somme = somme + note
+    
+    moyenne = somme / len(liste_notes)
     return moyenne
 
 
-def trouverMax(listeNotes):
-    if listeNotes==[]:
+def trouver_max(liste_notes):
+    """
+    Retourne la valeur maximale d'une liste.
+    Retourne None si la liste est vide.
+    """
+    if not liste_notes:
         return None
-    return max(listeNotes)
+    return max(liste_notes)
 
 
-def min_value(notes):
-    minimum = notes[0]
-    for i in notes:
-        if i<minimum:
-            minimum=i
+def min_value(liste_notes):
+    """
+    Retourne la valeur minimale d'une liste.
+    Retourne None si la liste est vide.
+    """
+    if not liste_notes:
+        return None
+
+    minimum = liste_notes[0]
+    for note in liste_notes:
+        if note < minimum:
+            minimum = note
+
     return minimum
