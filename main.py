624d27ee007e4ec14d2b952e9b7a46da4e2fbf9f
mmarwa fridi <mmarwafridi@gmail.com>
2026-02-25 13:23:11 +0000

    [add] test

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Fichier principal du programme StudentStats.
+"""
+
+import utils
+
+
+def main():
+    """
+    Fonction principale du programme.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,3 @@
+"""
+Module contenant les fonctions de statistiques des notes.
+"""
